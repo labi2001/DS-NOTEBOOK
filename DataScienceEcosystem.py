456a0b811ad68a957649812daf61240a4d89f76a
labi2001 <140580639+labi2001@users.noreply.github.com>
2023-12-28 13:49:40 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+# 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# #### OBJECTIVES 
+#     -> List examples of open-source tools used in data science.
+#     -> Identify the users and benefits of Python.
+#     -> List the various libraries: scientific, visualization, machine learning, and deep learning.
+#     -> Describe how to work in a notebook session.
+#     -> Demonstrate how to edit and upload files in GitHub.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. SQL
+# 3. R
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Pandas
+# 2. Numpy
+# 3. Matpltlib
+# 4. Seaborn
+
+# |Data Science Tools |
+# |:------------------|
+# |Jupyter Notebook   |
+# |R Studio           |
+# |Apache Zeppelin    |
+# 
+# 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+# This a simple arithmetic expression to mutiply then add integers.
+
+(3*4)+5
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by diving by 60.
+   
+# one_hour = 60 
+#  ?   =     200 
+
+(200 * 1) / 60
+
+
+# ## Author 
+# 
+# Omolabi Motunrayo
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
